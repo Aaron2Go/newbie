459e539af433d71cb613251c6692b9d700eef8ea
charlesmac <charles_mac@qq.com>
2018-04-27 19:01:26 +0800

    Fixed path bug

--- S3/models.py
+++ S3/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import os.path
 
 
 # Create your models here.
 class NavFile(models.Model):
     Project = models.ForeignKey('S2.Project', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='所属项目')
     InfoDate = models.DateField(verbose_name='口径日期')
-    File = models.FileField(upload_to='Upload/Nav_Tables', verbose_name='净值表Excel')
+    File = models.FileField(upload_to=os.path.join('Upload', 'Nav_Tables'), verbose_name='净值表Excel')
     UploadTime = models.DateTimeField(verbose_name='上传时间', auto_now=True, auto_created=True)
     Comments = models.TextField(verbose_name='备注', null=True, blank=True)
     objects = models.Manager()
@@ -19,7 +20,7 @@ class NavFile(models.Model):
 
 
 class PlBranch(models.Model):
-    File = models.FileField(upload_to='Upload/Branches', verbose_name='数据文件')
+    File = models.FileField(upload_to=os.path.join('Upload','Branches'), verbose_name='数据文件')
     UploadTime = models.DateTimeField(verbose_name='上传时间', auto_now=True, auto_created=True)
     Comments = models.TextField(verbose_name='备注', null=True, blank=True)
     objects = models.Manager()
@@ -33,7 +34,7 @@ class PlBranch(models.Model):
 
 
 class PlProject(models.Model):
-    File = models.FileField(upload_to='Upload/Projects', verbose_name='数据文件')
+    File = models.FileField(upload_to=os.path.join('Upload', 'Projects'), verbose_name='数据文件')
     InfoDate = models.DateField(verbose_name='口径日期')
     UploadTime = models.DateTimeField(verbose_name='上传时间', auto_now=True, auto_created=True)
     Comments = models.TextField(verbose_name='备注', null=True, blank=True)
